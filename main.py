c7cb5c719827797bee5f9204ccf22de675afcfb7
Nestor Mamani <nmamanipantoja@gmail.com>
2022-01-23 11:35:03 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+  firts_name: str
+  last_name: str
+  age: int
+  hair_color: Optional[str] = None
+  is_married: Optional[bool] = None
 
 @app.get('/')
 def home():
   return {'Hello': 'World'}
+
+
+#Request and response body
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)): # 3 puntos significa necesario
+  return Person
