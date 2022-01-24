c21ee6a6ffc5b7df4d60510906e5883de9a46b29
Nestor Mamani <nmamanipantoja@gmail.com>
2022-01-24 09:07:19 -0500

    validations_params

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -32,7 +32,29 @@ def create_person(person: Person = Body(...)): # 3 puntos significa necesario
 #Validaciones: Query Parameters
 @app.get('/person/details')
 def show_person(
-  name: Optional[str] = Query(None, min_length=1, max_length=50),
-  age: int = Query(..., ge=0, le=120)
+  name: Optional[str] = Query(
+    None, 
+    min_length=1, 
+    max_length=50, 
+    title='Person Name', 
+    description="This is the person name. It's between 1 and 50 char"
+    ),
+  age: int = Query(
+    ..., 
+    ge=0, 
+    le=120
+    )
 ):
   return {name: age}
+
+#Validaciones: Path Parameters
+@app.get('/person/detail/{person_id}')
+def show_person(
+  person_id: int = Path(
+    ..., 
+    gt=0,
+    title="Person Id Validation",
+    description="Return if a person_id exist"
+    )
+):
+  return {person_id: True}
